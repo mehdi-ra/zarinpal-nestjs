d1c3fe06f8ff9c288121490c2e0b10f50a3cfdfb
me-dira <mediraworkm@gmail.com>
2022-11-04 21:44:46 +0330

    delete: old nodejs code sample

--- ZPnode-master/run.js
+++
@@ -1,9 +0,0 @@
-var expressRoutes = require('./lib/expressRoutes.js');
-var appConfig = require('./appConfig');
-
-expressRoutes.accFiles();
-expressRoutes.userGUI();
-expressRoutes.adminPanel();
-expressRoutes.defaultRoutes();
-
-expressRoutes.appRun(appConfig.appPort);
